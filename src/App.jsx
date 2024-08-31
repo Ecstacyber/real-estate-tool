201694899111175f2a0b790d568c02cce34b595d
Duy Hao Nguyen <duyhao301202@gmail.com>
2024-08-31 19:19:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  DashboardOutlined
+} from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import './index.css'
+import { Link } from 'react-router-dom';
+
+const { Header, Content, Footer, Sider } = Layout;
+const siderStyle = {
+  overflow: 'auto',
+  height: '100vh',
+  position: 'fixed',
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  scrollbarWidth: 'thin',
+  scrollbarColor: 'unset',
+};
+
+// const items = [
+//   UserOutlined,
+//   VideoCameraOutlined,
+//   UploadOutlined,
+//   BarChartOutlined,
+//   CloudOutlined,
+//   AppstoreOutlined,
+//   TeamOutlined,
+//   ShopOutlined,
+// ].map((icon, index) => ({
+//   key: String(index + 1),
+//   icon: React.createElement(icon),
+//   label: `nav ${index + 1}`,
+// }));
+
+const navItems = [
+  {
+    key: '1',
+    icon: <DashboardOutlined />,
+    label: <Link to="/">Trang chủ</Link>,
+  }
+]
+
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout hasSider>
+      <Sider style={siderStyle}>
+        <div className="demo-logo-vertical"></div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={navItems} />
+      </Sider>
+      <Layout
+        style={{
+          marginInlineStart: 200,
+        }}
+      >
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '24px 16px 0',
+            overflow: 'initial',
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              textAlign: 'center',
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <p>long content</p>
+            {
+              // indicates very long content
+              Array.from(
+                {
+                  length: 100,
+                },
+                (_, index) => (
+                  <React.Fragment key={index}>
+                    {index % 20 === 0 && index ? 'more' : '...'}
+                    <br />
+                  </React.Fragment>
+                ),
+              )
+            }
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Real Estate Tool ©{new Date().getFullYear()} - Created by Blue Team
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
